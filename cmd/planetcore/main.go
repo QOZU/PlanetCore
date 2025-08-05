bc2224f3c8389c8e0125731829af4aff8e88721b
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-05 20:03:57 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/planetcore/main.go
@@ -0,0 +1,23 @@
+// cmd/planetcore/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"planetcore/internal/planetcore"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := planetcore.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
